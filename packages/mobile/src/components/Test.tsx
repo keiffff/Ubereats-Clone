60dcaef02e72bab6256405e1a4f593b85133a341
kei <tenguuenglish@gmail.com>
2020-07-26 21:26:05 +0900

    :wrench: Add setting for typescript path alias

--- packages/mobile/src/components/Test.tsx
+++ packages/mobile/src/components/Test.tsx
@@ -1,15 +1,13 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+export const Test = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Test</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
