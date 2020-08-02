9c369680b13c0b8c57985cee2183bc35f3f7ac41
kei <tenguuenglish@gmail.com>
2020-08-02 21:10:02 +0900

    :sparkles: Add landing screen

--- packages/mobile/src/screens/Landing/index.tsx
+++ packages/mobile/src/screens/Landing/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Image, Text } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { styles } from './styles';
 
 type Props = {
   onPressLogin: () => void;
@@ -8,12 +9,16 @@ type Props = {
 
 export const Landing = ({ onPressLogin }: Props) => {
   return (
-    <View>
-      <Image source={require('assets/restaurant-menu-green-icon.png')} />
-      <Text>Welcome to our restaurant</Text>
-      <Text>Order food and make reservations with one click.</Text>
-      <TouchableOpacity onPress={onPressLogin}>
-        <Text>Get Started</Text>
+    <View style={styles.base}>
+      <Image source={require('assets/restaurant-menu-green-icon.png')} style={styles.menuImage} />
+      <View style={styles.captionContainer}>
+        <Text style={styles.caption}>Welcome to our restaurant</Text>
+      </View>
+      <View style={styles.captionSubContainer}>
+        <Text style={styles.captionSub}>{`Order food and make reservations \n with one click.`}</Text>
+      </View>
+      <TouchableOpacity onPress={onPressLogin} style={styles.loginButton}>
+        <Text style={styles.loginButtonText}>Get Started</Text>
       </TouchableOpacity>
     </View>
   );
--- packages/mobile/src/screens/Landing/styles.ts
+++ packages/mobile/src/screens/Landing/styles.ts
@@ -1,5 +1,37 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  base: {},
+  base: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  menuImage: {
+    marginBottom: 40,
+  },
+  captionContainer: {
+    marginBottom: 24,
+  },
+  caption: {
+    color: '#5ea33a',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  captionSubContainer: {
+    marginBottom: 32,
+  },
+  captionSub: {
+    textAlign: 'center',
+  },
+  loginButton: {
+    backgroundColor: '#5ea33a',
+    borderRadius: 24,
+    paddingVertical: 12,
+    width: 200,
+  },
+  loginButtonText: {
+    textAlign: 'center',
+    color: '#ffffff',
+    fontWeight: 'bold',
+  },
 });
