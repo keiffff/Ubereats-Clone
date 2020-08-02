f49b3b4e3192ca23ecc39c2ab2b94fc83790ce69
kei <tenguuenglish@gmail.com>
2020-08-02 19:04:00 +0900

    :recycle: Make route name as constants

--- packages/mobile/src/routes/Main/Home.tsx
+++ packages/mobile/src/routes/Main/Home.tsx
@@ -3,13 +3,14 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { HamburgerButton } from '../Header/HamburgerButton';
 import { ShoppingCartButton } from '../Header/ShoppingCartButton';
 import { Home } from 'screens/Home';
+import { routes } from 'constants/routes';
 
 const Stack = createStackNavigator();
 
 export const HomeNavigator = () => (
   <Stack.Navigator>
     <Stack.Screen
-      name="Home"
+      name={routes.home}
       component={Home}
       options={{
         headerLeft: () => <HamburgerButton />,
--- packages/mobile/src/routes/Main/index.tsx
+++ packages/mobile/src/routes/Main/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { HomeNavigator } from './Home';
+import { routes } from 'constants/routes';
 
 const DrawerStack = createDrawerNavigator();
 
 export const Main = () => (
   <DrawerStack.Navigator>
-    <DrawerStack.Screen name="Home" component={HomeNavigator} />
+    <DrawerStack.Screen name={routes.home} component={HomeNavigator} />
   </DrawerStack.Navigator>
 );
