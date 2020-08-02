a84d051de216de2edb3f0c1010718c666f7abfcd
kei <tenguuenglish@gmail.com>
2020-08-02 21:16:18 +0900

    :sparkles: Enable to logout from menu

--- packages/mobile/src/routes/Drawer/LogoutItem.tsx
+++ packages/mobile/src/routes/Drawer/LogoutItem.tsx
@@ -3,16 +3,22 @@ import { DrawerItem } from '@react-navigation/drawer';
 import { routes } from 'constants/routes';
 import { LogoutIcon } from './LogoutIcon';
 import { Alert } from 'react-native';
+import { useAuth } from 'providers/Auth';
 
 type Props = Omit<ComponentProps<typeof DrawerItem>, 'label' | 'icon' | 'onPress'>;
 
 export const LogoutItem = (props: Props) => {
+  const { handleLogout } = useAuth();
+
   return (
     <DrawerItem
       label={routes.logout}
       icon={() => <LogoutIcon />}
       onPress={() => {
-        Alert.alert('Are you sure you want to log out ?');
+        Alert.alert('Log out', 'Are you sure you want to log out?', [
+          { text: 'OK', onPress: handleLogout },
+          { text: 'Cancel' },
+        ]);
       }}
       {...props}
     />
