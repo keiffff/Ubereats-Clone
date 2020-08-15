c1208248de42caf0221013980a0ddddfb389b3f2
kei <tenguuenglish@gmail.com>
2020-08-16 06:38:07 +0900

    :lipstick: Style cart screen

--- packages/mobile/src/screens/Cart/index.tsx
+++ packages/mobile/src/screens/Cart/index.tsx
@@ -1,20 +1,38 @@
-import React from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import React, { useMemo } from 'react';
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
 import { useCurrentCart } from 'providers/CurrentCart';
 
 export const Cart = () => {
   const { cartFoods } = useCurrentCart();
+  const totalPrice = useMemo(() => cartFoods.reduce((acc, { food, count }) => acc + food.price * count, 0), [
+    cartFoods,
+  ]);
 
   return (
-    <ScrollView style={styles.base}>
-      {cartFoods.map(({ food, count }, i) => (
-        <View key={i}>
-          <Text>{count}</Text>
-          <Text>{food.name}</Text>
-          <Text>{food.price}</Text>
+    <>
+      <ScrollView style={styles.base}>
+        <View style={styles.cartFoodsContainer}>
+          {cartFoods.map(({ food, count }, i) => (
+            <View key={i} style={styles.cartFood}>
+              <View style={styles.cartFoodInfo}>
+                <View style={styles.cartFoodCount}>
+                  <Text style={styles.cartFoodCountText}>{count}</Text>
+                </View>
+                <Text style={styles.cartFoodNameText}>{food.name}</Text>
+              </View>
+              <Text style={styles.cartFoodPriceText}>${food.price}</Text>
+            </View>
+          ))}
+          <View style={styles.cartFood}>
+            <Text style={styles.cartFoodNameText}>Total</Text>
+            <Text style={styles.cartFoodPriceText}>${totalPrice}</Text>
+          </View>
         </View>
-      ))}
-    </ScrollView>
+      </ScrollView>
+      <TouchableOpacity style={styles.placeOrderButton}>
+        <Text style={styles.placeOrderButtonText}>PLACE ORDER</Text>
+      </TouchableOpacity>
+    </>
   );
 };
--- packages/mobile/src/screens/Cart/styles.ts
+++ packages/mobile/src/screens/Cart/styles.ts
@@ -6,4 +6,46 @@ export const styles = StyleSheet.create({
     backgroundColor: '#ffffff',
     paddingVertical: 24,
   },
+  cartFoodsContainer: {
+    paddingHorizontal: 16,
+  },
+  cartFood: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+  },
+  cartFoodInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  cartFoodCount: {
+    borderWidth: 1,
+    borderColor: '#d8d8d8',
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    marginRight: 8,
+  },
+  cartFoodCountText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#5ea33a',
+  },
+  cartFoodNameText: {
+    fontSize: 16,
+  },
+  cartFoodPriceText: {
+    fontSize: 16,
+  },
+  placeOrderButton: {
+    marginTop: 'auto',
+    backgroundColor: '#5ea33a',
+    paddingVertical: 12,
+  },
+  placeOrderButtonText: {
+    textAlign: 'center',
+    color: '#ffffff',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
 });
