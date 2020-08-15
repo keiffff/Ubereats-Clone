6f2c8332b460c20ee758247f407e7457c3fe9341
kei <tenguuenglish@gmail.com>
2020-08-15 17:53:49 +0900

    :sparkles: Add cart foods when cart already exists

--- packages/mobile/src/screens/Food/index.tsx
+++ packages/mobile/src/screens/Food/index.tsx
@@ -3,13 +3,14 @@ import { ScrollView, View, Text, ActivityIndicator } from 'react-native';
 import { RouteProp, useRoute } from '@react-navigation/native';
 import { useQuery, useMutation } from '@apollo/client';
 import { StackParamList } from 'types/navigation';
-import { GetFoodByUuidDocument, CreateCartDocument } from './index.graphql';
+import { GetFoodByUuidDocument, CreateCartDocument, CreateCartFoodDocument } from './index.graphql';
 import { FoodDetail } from 'components/FoodDetail';
 import { Counter } from 'components/Counter';
 import { styles } from './styles';
 import { LoadingView } from 'components/LoadingView';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { useCurrentUser } from 'providers/CurrentUser';
+import { useCurrentCart } from 'providers/CurrentCart';
 
 type NavigationProp = {
   route: RouteProp<StackParamList, 'FOOD'>;
@@ -20,25 +21,38 @@ const MAX_COUNT_TO_ADD_TO_CART = 99;
 export const Food = () => {
   const { params } = useRoute<NavigationProp['route']>();
   const { userId } = useCurrentUser();
+  const { cartUuid } = useCurrentCart();
   const { loading: getFoodByUuidLoading, data } = useQuery(GetFoodByUuidDocument, {
     variables: { uuid: params.foodUuid },
   });
   const [createCart, { loading: createCartLoading }] = useMutation(CreateCartDocument);
+  const [createCartFood, { loading: createCartFoodLoading }] = useMutation(CreateCartFoodDocument);
   const [count, setCount] = useState(0);
   const handlePressIncrement = useCallback(() => setCount((prev) => prev + 1), []);
   const handlePressDecrement = useCallback(() => setCount((prev) => prev - 1), []);
   const totalPrice = useMemo(() => count * (data?.foods_by_pk?.price ?? 0), [count, data?.foods_by_pk?.price]);
   const handlePressAddToCart = useCallback(() => {
     if (!data?.foods_by_pk?.uuid || !count) return;
-    createCart({
-      variables: {
-        userId,
-        cartFoods: {
-          data: [{ food_uuid: data.foods_by_pk.uuid, count }],
+    const foodUuid = data.foods_by_pk.uuid;
+    if (cartUuid) {
+      createCartFood({
+        variables: {
+          cartUuid,
+          foodUuid,
+          count,
         },
-      },
-    });
-  }, [data?.foods_by_pk, count, createCart, userId]);
+      });
+    } else {
+      createCart({
+        variables: {
+          userId,
+          cartFoods: {
+            data: [{ food_uuid: foodUuid, count }],
+          },
+        },
+      });
+    }
+  }, [data?.foods_by_pk, count, createCart, userId, cartUuid, createCartFood]);
 
   return getFoodByUuidLoading || !data?.foods_by_pk ? (
     <LoadingView />
@@ -66,10 +80,10 @@ export const Food = () => {
         <View style={styles.addToCartButtonContainer}>
           <TouchableOpacity
             style={styles.addToCartButton}
-            disabled={!count || createCartLoading}
+            disabled={!count || createCartLoading || createCartFoodLoading}
             onPress={handlePressAddToCart}
           >
-            {!createCartLoading ? (
+            {!createCartLoading && !createCartFoodLoading ? (
               <Text style={styles.addToCartButtontext}>Add to Cart</Text>
             ) : (
               <ActivityIndicator color="#ffffff" />
