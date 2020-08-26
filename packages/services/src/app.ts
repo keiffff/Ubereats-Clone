c7c8e3f22d2914c33f1dd412f73678448b37f3cb
kei <tenguuenglish@gmail.com>
2020-08-26 12:58:58 +0900

    :sparkles: Refund payment when canceled

--- packages/services/src/app.ts
+++ packages/services/src/app.ts
@@ -12,9 +12,13 @@ app.get('/ping', (req, res) => {
   res.send('OK');
 });
 
-app.post('/order_canceled', (req, res) => {
+app.post('/order_canceled', async (req, res) => {
   const { event } = req.body;
-  if (event.data.op !== 'UPDATE' || event.data.new.status !== 'canceled') return;
+  if (event.data.op !== 'UPDATE' || event.data.old.status === 'canceled' || event.data.new.status !== 'canceled')
+    return;
+
+  const paymentIntent = await stripe.paymentIntents.retrieve(event.data.new.payment_secret);
+  await stripe.refunds.create({ payment_intent: paymentIntent.id });
 
   res.status(200).send({});
 });
