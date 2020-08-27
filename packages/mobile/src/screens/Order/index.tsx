99bf8655c95f64aa3074956b6247903fc5091e2d
kei <tenguuenglish@gmail.com>
2020-08-27 22:45:45 +0900

    :sparkles: Enable to get ordered foods in order page

--- packages/mobile/src/screens/Order/index.tsx
+++ packages/mobile/src/screens/Order/index.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
-import { ScrollView, Text } from 'react-native';
+import React, { useMemo } from 'react';
+import { ScrollView, Text, View, Image } from 'react-native';
 import { RouteProp, useRoute } from '@react-navigation/native';
+import { useQuery } from '@apollo/client';
 import { StackParamList } from 'types/navigation';
+import { GetOrderFoodsByOrderUuidDocument } from './index.graphql';
+import { styles } from './styles';
+import { LoadingView } from 'components/LoadingView';
 
 type NavigationProp = {
   route: RouteProp<StackParamList, 'ORDER'>;
@@ -9,11 +13,37 @@ type NavigationProp = {
 
 export const Order = () => {
   const { params } = useRoute<NavigationProp['route']>();
+  const { data, loading } = useQuery(GetOrderFoodsByOrderUuidDocument, { variables: { orderUuid: params.orderUuid } });
+  const totalPrice = useMemo(
+    () => data?.order_foods.reduce((acc, { food, count }) => acc + food.price * count, 0) ?? 0,
+    [data?.order_foods],
+  );
 
-  return (
-    <ScrollView>
-      <Text>Preparing your order...</Text>
-      <Text>{params.orderUuid}</Text>
+  return loading || !data?.order_foods ? (
+    <LoadingView />
+  ) : (
+    <ScrollView style={styles.base}>
+      <View style={styles.orderStatusContainer}>
+        <Text style={styles.orderStatusText}>Preparing your order...</Text>
+      </View>
+      <View style={styles.orderImageContainer}>
+        <Image source={require('assets/cooking-icon.png')} style={styles.orderImage} />
+      </View>
+      <View style={styles.orderSummaryContainer}>
+        <Text style={styles.orderSummaryCaption}>Order Summary</Text>
+        {data.order_foods.map(({ food, count }, i) => (
+          <View key={i} style={styles.orderedItem}>
+            <View style={styles.orderedCount}>
+              <Text>{count}</Text>
+            </View>
+            <Text numberOfLines={1}>{food.name}</Text>
+          </View>
+        ))}
+        <View style={styles.totalPriceContainer}>
+          <Text style={styles.totalPriceText}>Total</Text>
+          <Text style={styles.totalPrice}>${totalPrice}</Text>
+        </View>
+      </View>
     </ScrollView>
   );
 };
---
+++ packages/mobile/src/screens/Order/styles.ts
@@ -0,0 +1,58 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  base: {
+    flex: 1,
+    paddingVertical: 16,
+    backgroundColor: '#ffffff',
+  },
+  orderStatusContainer: {
+    paddingHorizontal: 16,
+  },
+  orderStatusText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  orderImageContainer: {
+    paddingVertical: 72,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  orderImage: {
+    width: 120,
+    height: 'auto',
+    aspectRatio: 1,
+  },
+  orderSummaryContainer: {
+    padding: 16,
+  },
+  orderSummaryCaption: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 25,
+  },
+  orderedItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  orderedCount: {
+    backgroundColor: '#d8d8d8',
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    marginRight: 8,
+  },
+  totalPriceContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  totalPriceText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  totalPrice: {
+    fontSize: 16,
+  },
+});
