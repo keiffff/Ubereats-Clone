b4cfdbf34a37f05e11cecfa35e498cf0dfa32a52
kei <tenguuenglish@gmail.com>
2020-10-11 12:45:33 +0900

    :recycle: Fix compile error

--- packages/mobile/src/constants/environment.ts
+++ packages/mobile/src/constants/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  hasuraGraphqlEndpoint: process.env.HASURA_GRAPHQL_ENDPOINT,
-  authDomain: process.env.AUTH_DOMAIN,
-  authClientId: process.env.AUTH_CLIENT_ID,
-  authAudience: process.env.AUTH_AUDIENCE,
+  hasuraGraphqlEndpoint: process.env.HASURA_GRAPHQL_ENDPOINT ?? '',
+  authDomain: process.env.AUTH_DOMAIN ?? '',
+  authClientId: process.env.AUTH_CLIENT_ID ?? '',
+  authAudience: process.env.AUTH_AUDIENCE ?? '',
 } as const;
